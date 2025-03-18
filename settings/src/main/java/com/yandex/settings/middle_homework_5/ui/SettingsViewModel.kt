3ddbff0bbfdbd7d37bb704189790b2bbd369c4d1
AleksandrRegulus <kashkarov@mail.ru>
2025-03-18 21:21:15 +0300

    Created Settings module

---
+++ settings/src/main/java/com/yandex/settings/middle_homework_5/ui/SettingsViewModel.kt
@@ -0,0 +1,22 @@
+package com.yandex.settings.middle_homework_5.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yandex.settings.middle_homework_5.data.data_store.SettingContainer
+import com.yandex.settings.middle_homework_5.ui.contract.DataStoreService
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(
+    private val dataStoreService: DataStoreService
+) : ViewModel() {
+
+    fun saveSetting(periodic: Long, delayed: Long) {
+        viewModelScope.launch {
+            dataStoreService.saveSetting(periodic = periodic, delayed = delayed)
+        }
+    }
+
+    fun getCurrentSetting(): SettingContainer {
+        return dataStoreService.settingData.value
+    }
+}
